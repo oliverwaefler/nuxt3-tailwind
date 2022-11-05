7d4b41588d8c77757c27debc22fad2715176bfc8
azholwa <oliver.waefler@aspectra.ch>
2022-11-05 09:26:10 +0100

    bla

--- app.vue
+++ app.vue
@@ -1,7 +1,4 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <h1 class="text-3xl font-bold text-blue-500">
-      How to install Tailwind CSS 3 in Nuxt 3
-    </h1>
-  </div>
+<comp-1/>
+<comp-2/>
 </template>

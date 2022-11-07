57f663300061f915990b5dc8e5c6b3e3aa45217f
azholwa <oliver.waefler@aspectra.ch>
2022-11-07 11:50:03 +0100

    add landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,55 @@
 <template>
-  <a href="/page1">page1</a>
-  <br>
-  <a href="/page2">page2</a>
+  <!-- ====== Cards Section Start -->
+  <section class="bg-[#F3F4F6] pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+    <div class="container mx-auto">
+      <div class="-mx-4 flex flex-wrap">
+        <div class="w-full px-4 md:w-1/2 xl:w-1/3">
+          <div class="mb-10 overflow-hidden rounded-lg bg-white">
+
+            <div class="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+              <h3>
+                <a
+                    href="javascript:void(0)"
+                    class="text-dark hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
+                >Vorschlag 1
+                </a>
+              </h3>
+              <a
+               href="/page1"
+                  class="text-body-color hover:border-primary hover:bg-primary inline-block rounded-full border border-[#E5E7EB] py-2 px-7 text-base font-medium transition hover:text-white"
+              >
+                Los gehts
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="w-full px-4 md:w-1/2 xl:w-1/3">
+          <div class="mb-10 overflow-hidden rounded-lg bg-white">
+
+            <div class="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+              <h3>
+                <a
+                    href="javascript:void(0)"
+                    class="text-dark hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
+                >Vorschlag 2
+                </a>
+              </h3>
+
+              <a
+                  href="/page2"
+                  class="text-body-color hover:border-primary hover:bg-primary inline-block rounded-full border border-[#E5E7EB] py-2 px-7 text-base font-medium transition hover:text-white"
+              >
+               Los gehts
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ====== Cards Section End -->
+
+
 </template>
 
 <script>
